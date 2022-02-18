3d5d048049099085c6154e0c5f335d3346f85575
Nikolay Miroshkin <gaudrad@gmail.com>
2022-02-18 10:40:08 +0300

    task_4 check

--- task_2/solution.js
+++ task_2/solution.js
@@ -82,10 +82,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     
     let freeShipping;
     freeShipping = !shippingSum;
-    //(shippingSum == 0) ? freeShipping = true : freeShipping = false;
-
-
-
+    
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
